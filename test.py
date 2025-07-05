7635cdcedf1e832dc7d1d87a24e94a60fb7f0b74
fatemeh <fatemehrouhafzaei.86@gmail.com>
2025-07-05 14:20:51 +0430

    a file for testing defs

---
+++ test.py
@@ -0,0 +1,23 @@
+grades = {
+
+    'riazi': 78,
+
+    'zist': 45,
+
+    'phizik': 62,
+
+    'shimi': 83
+}
+o = list(grades.values())
+if o[1] > 30 :
+    print("j")
+else :
+    print("o")    
+# if grades <= 50 :
+#     print ("poor")
+# elif 50 < grades <= 75 :
+#     print ("medium")
+# elif 75 < grades <= 100 :
+#     print("good") 
+# else :
+#     print("error")
