7a1e7d75371e1dfebf5ca98f988f98978d2bffe8
fatemeh <fatemehrouhafzaei.86@gmail.com>
2025-07-05 14:28:46 +0430

    create def dict_to_list

--- lib.py
+++ lib.py
@@ -1,13 +1,3 @@
 
-#def study_plan_generator(grades, target) :
-# def grades_level(grades) :
-
-#     for x, obj in grades.items():
-#         if grades <= 50 :
-#             return "poor"
-#         elif 50 < grades <= 75 :
-#             return "medium"
-#         elif 75 < grades <= 100 :
-#             return "good"
-#         else :
-#             print("error")
+def dict_to_list(grades) :
+    return(list(grades.values()))
