f883a8aa362c8b8d04f9733fcb177cd9d4f1536e
fatemeh <fatemehrouhafzaei.86@gmail.com>
2025-07-05 14:20:18 +0430

    craete a file for def libs

---
+++ lib.py
@@ -0,0 +1,13 @@
+
+#def study_plan_generator(grades, target) :
+# def grades_level(grades) :
+
+#     for x, obj in grades.items():
+#         if grades <= 50 :
+#             return "poor"
+#         elif 50 < grades <= 75 :
+#             return "medium"
+#         elif 75 < grades <= 100 :
+#             return "good"
+#         else :
+#             print("error")
