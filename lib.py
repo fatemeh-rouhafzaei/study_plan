877629f95f57cd65f0079ce38950eadfe8f50e19
fatemeh <fatemehrouhafzaei.86@gmail.com>
2025-07-05 15:00:47 +0430

    def grades_level

--- lib.py
+++ lib.py
@@ -1,2 +1,12 @@
+def grades_level (grades) :
+        for key , Value in grades.items() :
+            if 0 <= Value <= 50 :
+                print("grades poor" , "=", key , ":" , Value)
+            elif 50 < Value <= 75 :
+                print("grades medium" , "=", key , ":" , Value)
+            elif 75 < Value <= 100 :
+                print("grades good" , "=", key , ":" , Value)
+            else :
+                 print("error")
 
 
